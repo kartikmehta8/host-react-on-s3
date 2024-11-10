a9a1fe76ef30df381ea3f524581a0f4181e8918c
Kartik Mehta <kartikmehta202@gmail.com>
2024-11-10 14:25:34 +0530

    Prettify

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function App() {
   return (
     <div>
       <h1>Hello, React with Parcel!</h1>
-      <p>This is a minimal setup for a React app using Parcel hosted on Amazon S3.</p>
+      <p>
+        This is a minimal setup for a React app using Parcel hosted on Amazon
+        S3.
+      </p>
     </div>
   );
 }
