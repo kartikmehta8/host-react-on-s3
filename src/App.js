5170e1c8e61cb34992442a8b543a09e51537b1f4
Kartik Mehta <kartikmehta202@gmail.com>
2024-11-10 14:26:33 +0530

    Update heading

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, React with Parcel!</h1>
+      <h1>Hello, React with Parcel on S3!</h1>
       <p>
         This is a minimal setup for a React app using Parcel hosted on Amazon
         S3.
